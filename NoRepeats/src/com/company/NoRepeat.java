8157877a8bfbe9518697e010a34c6198bd74e8fd
Ditebogo50 <69639490+Ditebogo50@users.noreply.github.com>
2022-04-05 11:54:19 +0200

    Add files via upload
    Added files for the program

---
+++ NoRepeats/src/com/company/NoRepeat.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.ArrayList;
+
+
+public class NoRepeat {
+
+    public static void main(String[] args) {
+        //String to test the method
+        String input = "And I think to myself: what a wonderful world!";
+
+        //Call the method and save in variable
+        String result = removeRepeats(input);
+
+        //Print out the results
+        System.out.println(input);
+        System.out.println(result.toString());
+    }
+
+    public static String removeRepeats(String originalString) {
+
+        //Initialize char array and store the string cast to the array
+        char[] originalArray = originalString.toCharArray();
+
+        ArrayList<Character> resultArray = new ArrayList<Character>();
+
+        //Loop to Check for repeats and store non repeats in the array list
+        for (int i = 0; i < originalArray.length ; i++) {
+            if(!resultArray.toString().toLowerCase().contains(String.valueOf(originalArray[i]).toLowerCase()) || originalArray[i] == ' ') {
+                resultArray.add(originalArray[i]);
+            }
+        }
+
+        //Cast the array to a string
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Character ch : resultArray) {
+            stringBuilder.append(ch);
+        }
+        return stringBuilder.toString();
+    }
+}
+
